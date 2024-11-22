51e68ebbba4e289bc67a42af81dd68963bee0663
VIPJANIYA <rmkarunarathna052@gmail.com>
2024-11-22 18:53:56 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=TQNB2DKA#8vfVIIxC0jdevMEpcz-EeZeKujDLfk6ssL3q6vVbS3Q",
 };
